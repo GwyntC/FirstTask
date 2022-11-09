1afdb30915e933ef3caf12c067f939bcdef07801
GwyntC <m.drozdov1998@gmail.com>
2022-11-09 17:21:31 +0300

    Second Commit: Add classes and methods for all tasks. Add package for Shapes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[][] lines={
+                {"Mr","#Joy","#Jack","#Joy"},
+                {"#Joy","#Jac","#Jack","#Jil","#Luce","#Sam"},
+                {"#Joy","#Jack","#j"}
+        };
+        SecondTask task=new SecondTask();
+        Map<String,Integer> res=task.getTopTags(task.getTags(lines));
+        for (Map.Entry<String, Integer> entry : res.entrySet()) {
+            System.out.println(entry.getKey() + ":" + entry.getValue().toString());
+        }
     }
 }
